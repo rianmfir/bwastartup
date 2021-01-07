ec5b03ddd4ea97cd2002836ffc4a5e9a15206d3f
rianmfir <rianmfirdaus@gmail.com>
2021-01-07 08:53:37 +0700

    Email checking endpoint

--- main.go
+++ main.go
@@ -28,7 +28,7 @@ func main() {
 
 	api.POST("/users", userHandler.RegisterUser)
 	api.POST("/sessions", userHandler.Login)
-	api.POST("/email_checker", userHandler.CheckEmailAvailability)
+	api.POST("/email_checkers", userHandler.CheckEmailAvailability)
 
 	router.Run(":7000")
 }
