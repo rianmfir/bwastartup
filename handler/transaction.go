1293060c10c2cbdef1f046d4f559dcfe5ca72bbb
rianmfir <rianmfirdaus@gmail.com>
2021-04-19 19:23:58 +0700

    Transaction - Membuat Handler

--- handler/transaction.go
+++ handler/transaction.go
@@ -1,7 +1,38 @@
 package handler
 
-// parameter di uri
-// tangkap parameter mapping input struct
-// panggil service, input struct sebagai parameter
-// service berbekal campaign id bisa panggil repo
-// repo mencari data transaction suatu campaign
+import (
+	"BWA_Startup/helper"
+	"BWA_Startup/transaction"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+type transactionHandler struct {
+	service transaction.Service
+}
+
+func NewTransactionHandler(service transaction.Service) *transactionHandler {
+	return &transactionHandler{service}
+}
+
+func (h *transactionHandler) GetCampaignTransactions(c *gin.Context) {
+	var input transaction.GetCampaignTransactionsinput
+
+	err := c.ShouldBindUri(&input)
+	if err != nil {
+		response := helper.APIResponse("Failde to get campaign's transactions", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	transactions, err := h.service.GetTransactionByCampaignID(input)
+	if err != nil {
+		response := helper.APIResponse("Failde to get campaign's transactions", http.StatusBadRequest, "error", nil)
+		c.JSON(http.StatusBadRequest, response)
+		return
+	}
+
+	response := helper.APIResponse("Campaign transaction", http.StatusBadRequest, "success", transactions)
+	c.JSON(http.StatusOK, response)
+}
