f7c00d057f0fa9cf1b0a49cb0e42a8e1e1b30e95
rianmfir <rianmfirdaus@gmail.com>
2021-04-22 13:37:59 +0700

    User Transaction - Service

--- transaction/service.go
+++ transaction/service.go
@@ -12,6 +12,7 @@ type service struct {
 
 type Service interface {
 	GetTransactionByCampaignID(input GetCampaignTransactionsinput) ([]Transaction, error)
+	GetTransactionByUserID(userID int) ([]Transaction, error)
 }
 
 func NewService(repository Repository, campaignRepository campaign.Repository) *service {
@@ -29,6 +30,15 @@ func (s *service) GetTransactionByCampaignID(input GetCampaignTransactionsinput)
 	}
 
 	transactions, err := s.repository.GetByCampaignID(input.ID)
+	if err != nil {
+		return transactions, err
+	}
+
+	return transactions, nil
+}
+
+func (s *service) GetTransactionByUserID(userID int) ([]Transaction, error) {
+	transactions, err := s.repository.GetByUserID(userID)
 	if err != nil {
 		return transactions, err
 	}
